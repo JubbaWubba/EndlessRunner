6e8c1492cad6796903a784b9e5ec4a5a13d184c2
JubbaWubba <gabrielmaghanoyr@gmail.com>
2022-04-19 15:50:56 -0700

    format

---
+++ src/prefabs/Player.js
@@ -0,0 +1,11 @@
+// Player prefab
+class Player extends Phaser.GameObjects.Sprite {
+    constructor(scene, x, y, texture, frame,) {
+        super(scene, x, y, texture, frame);
+        scene.add.existing(this);   // add to existing scene
+    }
+
+    update() {
+
+    }
+}
