7723cc5ab5e7fbea62a246f79119eb28cb2a3ef9
JubbaWubba <gabrielmaghanoyr@gmail.com>
2022-05-03 16:11:02 -0700

    JS comments

--- src/main.js
+++ src/main.js
@@ -13,3 +13,5 @@ let borderPadding = borderUISize / 3;
 // reserve keyboard vars
 let keyF, keyR, keyLEFT, keyRIGHT, KeyUp, keyDown, keyA, keyD, keyW, keyS;
 
+// Our artstyle was meant to emulate vintage arcade games along with sounds to match a mad-max sort of arcade game feel
+// Technically interesting, the way our game makes use of timers to both increase the y-axis of players, does the speed up, and the distance score. 
